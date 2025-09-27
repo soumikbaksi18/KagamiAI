df20cac0d3c977bdb9f4c1149fa3d8a0b06be8be
soumikbaksi18 <baksisoumik@gmail.com>
2025-09-27 11:36:53 +0530

    Tailwind fixes added

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
   ],
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  css: {
+    postcss: './postcss.config.js',
   }
 })
